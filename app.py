f30b9e0b9425ae83cf8f118ee3c396d0679032f0
ranveersanjayupadhyay <ranveersanjayupadhyay@gmail.com>
2026-09-12 18:27:44 +0000

    .

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ except Exception as e:
     st.warning("Please upload your image and name it 'bg.jpg'")
 
 # 3. The Invisible Spacer to push elements below the logo
-st.markdown("<div style='height: 250px;'></div>", unsafe_allow_html=True)
+st.markdown("<div style='height: 300px;'></div>", unsafe_allow_html=True)
 st.markdown(" 👉[GET YOUR FREE API KEY HERE](https://aistudio.google.com/app/apikey)", unsafe_allow_html=True)
 api_key = st.text_input("ENTER YOUR GEMINI API KEY:", type="password")
 notes = st.text_area("PASTE YOUR STUDY NOTES HERE:", height=150)
